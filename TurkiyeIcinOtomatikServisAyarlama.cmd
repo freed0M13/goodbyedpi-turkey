7810b5892d8e78a7794cc1773faedb129753a1dc
Ozgur D. Ural <83591809+freed0M13@users.noreply.github.com>
2024-10-12 23:00:14 +0300

    Add files via upload

---
+++ TurkiyeIcinOtomatikServisAyarlama.cmd
@@ -0,0 +1,24 @@
+@ECHO OFF
+PUSHD "%~dp0"
+set _arch=x86
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
+IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
+
+echo Bu kurulum dosyasi yonetici izinleri verilerek calistirilmali.
+echo "Sag Tikla - Yonetici Olarak Calistir" yapmalisin.
+echo Eger yonetici olarak calistirdiysan herhangi bir tusa bas ve devam et.
+pause
+
+netsh interface ipv4 set dns "Wi-Fi" static 1.1.1.1 primary
+netsh interface ipv4 add dns "Wi-Fi" 1.0.0.1 index=2
+
+netsh interface ipv4 set dns "Ethernet" static 1.1.1.1 primary
+netsh interface ipv4 add dns "Ethernet" static 1.0.0.1 index=2
+
+sc stop "GoodbyeDPI"
+sc delete "GoodbyeDPI"
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" --set-ttl 3 start= auto
+sc description "GoodbyeDPI" "Pasif DPI Engelleyici | Turkiye ISPlerine Gore Ayarlanmis Halde"
+sc start "GoodbyeDPI"
+
+POPD
